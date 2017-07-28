b85bd972bfbb6ee13d070a3836e59b3a3926427c
Matías Schaab <schaab.matias@gmail.com>
2017-07-27 22:30:58 -0300

    Testeanding
    hu3

---
+++ UnityLudumDare39/Assets/scripts/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//hu3hu3hu3
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
