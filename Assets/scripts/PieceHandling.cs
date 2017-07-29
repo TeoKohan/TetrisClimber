cbbddded0ea0c7d29fa8a3a87e461d7b9251443e
Julia Morelli <jmlanghe@gmail.com>
2017-07-29 16:06:06 -0300

    Picking Pieces
    + You can now theoretically pick up a piece.
    + Implementation on player pending

---
+++ Assets/scripts/PieceHandling.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceHandling : MonoBehaviour {
+
+    [SerializeField]
+    private Transform piecePivot;
+    [SerializeField]
+    private float maxDistanceToInteract;
+    [SerializeField]
+    private LayerMask pieceLayer;
+    [SerializeField]
+    private LayerMask towerLayer;
+
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, maxDistanceToInteract, pieceLayer))
+            {
+                PickPiece(hit.collider.gameObject);
+            }
+
+        }
+    }
+
+
+    void PickPiece(GameObject piece)
+    {
+        piece.transform.SetParent(piecePivot);
+        piece.transform.localPosition = new Vector3(0,0,0);
+    }
+
+
+}
